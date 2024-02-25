e69b8e3e1b36668d6618a761b6bf1364456d0371
kaiguangg <kaiguangg@hotmail.com>
2024-02-25 16:30:10 +0800

    Initialise Spring

---
+++ src/test/java/com/demo/descope/DescopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.descope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DescopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
